82de59bbe1e2fb8eb61a1655a092e08e1e1a4b58
Anton <antonyvbazin@gmail.com>
2019-10-05 13:06:28 +0300

    some checks in main

--- main.cpp
+++ main.cpp
@@ -4,6 +4,9 @@
 int main() {
      std::cout << "Hello, World!" << std::endl;
      cpplab3v13::element elem;
-     cpplab3v13::element(3, 4);
+     cpplab3v13::element elem2(3, 4);
+     auto *ptr = new cpplab3v13::connection[5];
+     cpplab3v13::element elem3(ptr, 5);
+     delete [] ptr;
     return 0;
 }
