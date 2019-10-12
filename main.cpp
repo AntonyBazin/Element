abf643b568015fd37c17641533d6d74e8083efce
Antony <34607019+AntonyBazin@users.noreply.github.com>
2019-10-13 00:26:01 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,23 @@
 #include "element.h"
 
-int main() {
+using namespace cpplab3v13;
 
-    auto *ptr = new cpplab3v13::connection[7];
+int main() {
+    int rc;
+    auto *ptr = new connection[7];
+    int (*function_ptr[])(element&) = {nullptr, d_add_conn, d_del_conn,
+                                       d_show_all, d_disconnect_conn,
+                                       d_connect_conn,d_change_all_states,
+                                       d_print_conn_state, d_set_conn_state};
+
+
     for(int i = 0; i < 5; ++i){
-        ptr[i].type = i < 2 ? cpplab3v13::IN : cpplab3v13::OUT;
+        ptr[i].type = i < 2 ? IN : OUT;
     }
-    cpplab3v13::run(ptr, 5);
+    element elem(ptr, 5);
+    while((rc = dialog())){
+        if(!function_ptr[rc](elem)) break;
+    }
     delete [] ptr;
     return 0;
 }
